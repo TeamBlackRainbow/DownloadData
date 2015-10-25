40e232a1b4b777a70564568a1c16057b352bdb40
sprusr <sprusr@sprusr.tk>
2015-10-25 01:41:08 +0000

    Fixed stuff, made stuff happen

--- allindeces.js
+++ allindeces.js
@@ -5,10 +5,12 @@ var index2 = [ 'MNDI', 'MRW', 'NXT', 'OML', 'PSON', 'PSN', 'RRS', 'REL', 'RIO', 
 //We need to work with "MongoClient" interface in order to connect to a mongodb server.
 var MongoClient = mongodb.MongoClient;
 
+var led = require('./led-strip.js');
+
 // Connection URL. This is where your mongodb server is running.
 var url = 'mongodb://localhost:27017/bloomberg';
 
-var data1 = [];
+var things = [];
 
 // Use connect method to connect to the Server
 
@@ -20,58 +22,60 @@ aws.config.update({
 });
 var awsdb = new aws.DynamoDB.DocumentClient();
 
-for (i = 0; i < 100; i++) { 
-  MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    //HURRAY!! We are connected. :)
-    console.log('Connection established to', url);
+for (i = 0; i < 30; i++) {
 
-    // Get the documents collection
-    var collection = db.collection('Items');
+      //Create some users
 
-    //Create some users
-
-var params = {
-    TableName: '2013',
-    KeyConditionExpression: '#t = :ticker and #d between :date1 and :date2',
-    ExpressionAttributeNames: {
-  '#t': 'Ticker',
-  '#d': 'Date'
-    },
-    ExpressionAttributeValues: {
-  ':ticker': index2[i]+' LN Equity',
-  ':date1': '2013-01-01',
-  ':date2': '2013-06-02'
-    }
-};
-
-awsdb.query(params, function(err, data) {
-    if (err)
-  console.log(err, err.stack);
-    else
-  console.log(data['Items']);
-collection.insert(data['Items'], function (err, result) {
-      if (err) {
-        console.log(err);
+  var params = {
+      TableName: '2013',
+      KeyConditionExpression: '#t = :ticker and #d between :date1 and :date2',
+      ExpressionAttributeNames: {
+    '#t': 'Ticker',
+    '#d': 'Date'
+      },
+      ExpressionAttributeValues: {
+    ':ticker': index2[i]+' LN Equity',
+    ':date1': '2013-01-01',
+    ':date2': '2013-06-02'
       }
-      //Close connection
-      
-    });
+  };
 
-    // Insert some users
+  awsdb.query(params, function(err, data) {
+    if (err)
+      console.log(err, err.stack);
+    else
+      console.log(data['Items']);
 
-});
+      things.push(data['Items']);
 
+      // Insert some users
 
-
-db.close();
+  });
 
 }
- 
- 
-});
-}
+
+setTimeout(function() {
+  console.log(things);
+
+  for(var i = 1; i < 180; i++) {
 
 
+    for(var j = 0; j < 60; j++) {
+      if(things[j]) {
+        if(things[j][i]) {
+          if(things[j][i].High > things[j][i-1].High) {
+            led.setPixel(j, 0, 255, 0);
+          } else {
+            led.setPixel(j, 255, 0, 0);
+          }
+        } else {
+          led.setPixel(j, 0, 0, 255);
+        }
+      } else {
+        led.setPixel(j, 0, 0, 255);
+      }
+
+
+    }
+  }
+}, 2000);
